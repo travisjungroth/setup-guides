696b89fde93692ab75c1595ba1c56e76a4edc333
travisjungroth <jungroth@gmail.com>
2020-03-21 23:02:16 -0600

    Fix release command

--- scripts/release_tasks.sh
+++ scripts/release_tasks.sh
@@ -5,7 +5,7 @@ if [ -z "$HEROKU_SLUG_COMMIT" ]
 then
   curl -sL https://sentry.io/get-cli/ | bash
   sentry-cli releases new -p starter "$HEROKU_SLUG_COMMIT"
-  sentry-cli releases set-commits -c "$HEROKU_SLUG_COMMIT" "$HEROKU_SLUG_COMMIT"
+  sentry-cli releases set-commits "$HEROKU_SLUG_COMMIT" -c "travisjungroth/starter@$HEROKU_SLUG_COMMIT"
   sentry-cli releases finalize "$HEROKU_SLUG_COMMIT"
-  sentry-cli releases deploys "$HEROKU_SLUG_COMMIT" new -e "$HEROKU_APP_NAME"
+  sentry-cli releases deploys "$HEROKU_SLUG_COMMIT" new -e "$HEROKU_APP_NAME" -n "$HEROKU_APP_NAME-$HEROKU_RELEASE_VERSION"
 fi
