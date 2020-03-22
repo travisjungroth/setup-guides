79f388423d33a9086da104244295cb2c9473c096
travisjungroth <jungroth@gmail.com>
2020-03-21 22:43:00 -0600

    Fix commit setting

--- scripts/release_tasks.sh
+++ scripts/release_tasks.sh
@@ -4,6 +4,6 @@ if [ -z "$HEROKU_SLUG_COMMIT" ]
 then
   curl -sL https://sentry.io/get-cli/ | bash
   sentry-cli releases new -p starter "$HEROKU_SLUG_COMMIT"
-  sentry-cli releases set-commits --auto "$HEROKU_SLUG_COMMIT"
+  sentry-cli releases set-commits -c "$HEROKU_SLUG_COMMIT"
   sentry-cli releases finalize "$HEROKU_SLUG_COMMIT"
 fi
