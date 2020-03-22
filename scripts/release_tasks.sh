09cac30ffa915dd6dc2619acf90341214cb687c4
travisjungroth <jungroth@gmail.com>
2020-03-21 22:34:11 -0600

    Fix release tasks

--- scripts/release_tasks.sh
+++ scripts/release_tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py migrate --noinput || exit
-if [ -n "$HEROKU_SLUG_COMMIT" ]
+if [ -z "$HEROKU_SLUG_COMMIT" ]
 then
   curl -sL https://sentry.io/get-cli/ | bash
   sentry-cli releases new -p starter "$HEROKU_SLUG_COMMIT"
