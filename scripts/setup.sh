efac78f31870793b39882b9aaa23190770961beb
travisjungroth <jungroth@gmail.com>
2020-03-30 13:18:22 -0600

    Fix spacing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-db_name = "highly_composite"
+db_name=highly_composite
 password=$(openssl rand -hex 32)
 scripts/createdb.sh "${db_name}" "${password}"
 echo "DATABASE_URL=postgres://${db_name}_user:$password@localhost:5432/$db_name" >> .env
