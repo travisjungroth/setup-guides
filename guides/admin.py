238886c685e4bae2bb94fb7f5562a86583790ccf
travisjungroth <jungroth@gmail.com>
2020-04-01 17:56:15 -0600

    Reformat genereated admin

--- guides/admin.py
+++ guides/admin.py
@@ -1,40 +1,30 @@
 from django.contrib import admin
 
-from . import models
+from guides import models
 
 
+@admin.register(models.Guide)
 class GuideAdmin(admin.ModelAdmin):
-
     list_display = ('id', 'title', 'slug')
     list_filter = ('id', 'title', 'slug')
     raw_id_fields = ('steps',)
     search_fields = ('slug',)
 
 
+@admin.register(models.Step)
 class StepAdmin(admin.ModelAdmin):
-
     list_display = ('id', 'title')
     list_filter = ('id', 'title')
     raw_id_fields = ('requirements', 'actions', 'verifications')
 
 
+@admin.register(models.Action)
 class ActionAdmin(admin.ModelAdmin):
-
     list_display = ('id', 'text')
     list_filter = ('id', 'text')
 
 
+@admin.register(models.Verification)
 class VerificationAdmin(admin.ModelAdmin):
-
     list_display = ('id', 'text')
     list_filter = ('id', 'text')
-
-
-def _register(model, admin_class):
-    admin.site.register(model, admin_class)
-
-
-_register(models.Guide, GuideAdmin)
-_register(models.Step, StepAdmin)
-_register(models.Action, ActionAdmin)
-_register(models.Verification, VerificationAdmin)
