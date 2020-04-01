505e43b270933bc94854a40a75811b7e297ba26f
travisjungroth <jungroth@gmail.com>
2020-04-01 08:13:30 -0600

    Update Sentry project name

--- scripts/release_tasks.sh
+++ scripts/release_tasks.sh
@@ -6,7 +6,7 @@ then
   export INSTALL_DIR=/app
   curl -sL https://sentry.io/get-cli/ | bash
   ./sentry-cli releases new -p starter "$HEROKU_SLUG_COMMIT"
-  ./sentry-cli releases set-commits "$HEROKU_SLUG_COMMIT" -c "travisjungroth/starter@$HEROKU_SLUG_COMMIT"
+  ./sentry-cli releases set-commits "$HEROKU_SLUG_COMMIT" -c "travisjungroth/highly-composite@$HEROKU_SLUG_COMMIT"
   ./sentry-cli releases finalize "$HEROKU_SLUG_COMMIT"
   ./sentry-cli releases deploys "$HEROKU_SLUG_COMMIT" new -e "$HEROKU_APP_NAME" -n "$HEROKU_APP_NAME-$HEROKU_RELEASE_VERSION"
 fi
