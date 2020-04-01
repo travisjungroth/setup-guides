c951c397252f3aaf94fbc1bf4d1bfaa3e696f206
travisjungroth <jungroth@gmail.com>
2020-04-01 17:54:34 -0600

    Auto generate guide admin

--- guides/admin.py
+++ guides/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class GuideAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'slug')
+    list_filter = ('id', 'title', 'slug')
+    raw_id_fields = ('steps',)
+    search_fields = ('slug',)
+
+
+class StepAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title')
+    list_filter = ('id', 'title')
+    raw_id_fields = ('requirements', 'actions', 'verifications')
+
+
+class ActionAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'text')
+    list_filter = ('id', 'text')
+
+
+class VerificationAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'text')
+    list_filter = ('id', 'text')
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Guide, GuideAdmin)
+_register(models.Step, StepAdmin)
+_register(models.Action, ActionAdmin)
+_register(models.Verification, VerificationAdmin)
