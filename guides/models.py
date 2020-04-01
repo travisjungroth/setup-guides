1153b020057130dc0a442db4de5eacccc7d46364
travisjungroth <jungroth@gmail.com>
2020-04-01 17:29:22 -0600

    Build some sweet models

--- guides/models.py
+++ guides/models.py
@@ -1,25 +1,21 @@
 from django.db import models
+from sortedm2m.fields import SortedManyToManyField
 
 
 class Guide(models.Model):
     title = models.CharField(max_length=255)
-
-
-# A guide has steps in a particular order
+    steps = SortedManyToManyField('Step')
 
 
 class Step(models.Model):
     title = models.CharField(max_length=255)
-
-
-# A step has actions in a particular order
+    requirements = models.ManyToManyField('Step')
+    actions = SortedManyToManyField('Action')
+    verifications = SortedManyToManyField('Verification')
 
 
 class Action(models.Model):
     text = models.TextField()
-
-
-# A step has verifications in a particular order
 
 
 class Verification(models.Model):
