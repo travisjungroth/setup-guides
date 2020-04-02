c1dff50ba9dc71bd7da0272400cda46ec4af9572
travisjungroth <jungroth@gmail.com>
2020-04-01 18:48:56 -0600

    Pass at models with ordering

--- guides/models.py
+++ guides/models.py
@@ -5,20 +5,33 @@ from ordered_model.models import OrderedModel
 class Guide(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    # steps = SortedManyToManyField('Step', blank=True)
+    steps = models.ManyToManyField('Step', through='GuideStep', blank=True)
+
+
+class GuideStep(OrderedModel):
+    guide = models.ForeignKey('Guide', on_delete=models.CASCADE)
+    step = models.ForeignKey('Step', on_delete=models.CASCADE)
+    optional = models.BooleanField(default=False)
+    order_with_respect_to = 'guide'
 
 
 class Step(models.Model):
     title = models.CharField(max_length=255)
-    requirements = models.ManyToManyField('Step')
-    # verifications = SortedManyToManyField('Verification', blank=True)
+    requirements = models.ManyToManyField('self', through='Requirements', symmetrical=False, blank=True)
+
+
+class Requirements(OrderedModel):
+    required_by = models.ForeignKey('Step', on_delete=models.CASCADE)
+    requires = models.ForeignKey('Step', on_delete=models.CASCADE)
+    order_with_respect_to = 'requires'
 
 
 class Action(OrderedModel):
     text = models.TextField()
-    step = models.ForeignKey(Step, on_delete=models.PROTECT)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
     order_with_respect_to = 'step'
 
 
 class Verification(models.Model):
     text = models.TextField()
+    step = models.ForeignKey('Step', on_delete=models.CASCADE)
