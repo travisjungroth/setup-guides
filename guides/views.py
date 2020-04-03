c63a198c739f3034ec9dc4aff634c27d7738fd91
travisjungroth <jungroth@gmail.com>
2020-04-03 17:10:27 -0600

    Add a step detail page

--- guides/views.py
+++ guides/views.py
@@ -1,8 +1,11 @@
 from django.views.generic import DetailView
 
-from guides.models import Guide
+from guides.models import Guide, Step
 
 
 class GuideDetail(DetailView):
     model = Guide
 
+
+class StepDetail(DetailView):
+    model = Step
