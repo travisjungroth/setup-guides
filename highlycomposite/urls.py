5d0df83b255c86b51b0535f311a7153f94fc1984
Travis Jungroth <jungroth@gmail.com>
2020-04-03 17:36:42 -0600

    Add list pages for guides and steps (#55)
    * Add absolute URLs
    
    * Add list pages

--- highlycomposite/urls.py
+++ highlycomposite/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', highly_composite_views.Home.as_view(), name='home'),
     path('heartbeat/', highly_composite_views.heartbeat, name='heartbeat'),
+    path('guides/', guide_views.GuideList.as_view(), name='guide-list'),
     path('guides/<slug:slug>/', guide_views.GuideDetail.as_view(), name='guide-detail'),
+    path('steps/', guide_views.StepList.as_view(), name='step-list'),
     path('steps/<slug:slug>/', guide_views.StepDetail.as_view(), name='step-detail'),
 ]
